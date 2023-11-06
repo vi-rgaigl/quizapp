0b9743ffcd0ed985fb49946b56f3c68551af6915
Regina Gaigl <regina.gaigl@gmail.com>
2023-11-06 21:43:48 +0100

    welcome & data

---
+++ data.js
@@ -0,0 +1,104 @@
+let questions_physics = [
+    {
+        "question": "In welcher Einheit wird der elektrische Widerstand gemessen?",
+        "answer_1": "Watt",
+        "answer_2": "Volt",
+        "answer_2": "Ampere",
+        "answer_4": "Ohm",
+        "right_answer": 4
+    },
+    {
+        "question": "Welcher ist der einzige Planet, der die Sonne im Uhrzeigersinn umkreist?",
+        "answer_1": "Erde",
+        "answer_2": "Venus",
+        "answer_3": "Neptun",
+        "answer_4": "Uranus",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie lautet das chemische Symbol für Blei?",
+        "answer_1": "Bl",
+        "answer_2": "Pb",
+        "answer_3": "Be",
+        "answer_4": "Pt",
+        "right_answer": 2
+    }
+];
+let questions_history = [
+    {
+        "question": "Wer ist die griechische Götting der Weisheit in der Kriegsführung?",
+        "answer_1": "Aphrodite",
+        "answer_2": "Hera",
+        "answer_3": "Athene",
+        "answer_4": "Minerva",
+        "right_answer": 3
+    },
+    {
+        "question": "Welche Farbe wurde ursprünglich mit dem St. Patrick's Day assoziiert?",
+        "answer_1": "blau",
+        "answer_2": "grün",
+        "answer_3": "weiß",
+        "answer_4": "violett",
+        "right_answer": 1
+    },
+    {
+        "question": "in welchem Jahr ist Neil Armstrong auf dem Mond gelandet?",
+        "answer_1": "1963",
+        "answer_2": "1967",
+        "answer_3": "1969",
+        "answer_4": "Er ist nie auf dem Mond gelandet.",
+        "right_answer": 3
+    }
+];
+let questions_geography = [
+    {
+        "question": "Welche dieser Städte ist nicht die Hauptstadt eines Landes?",
+        "answer_1": "Wien",
+        "answer_2": "Delhi",
+        "answer_3": "Rio de Janeiro",
+        "answer_4": "Canberra",
+        "right_answer": 3
+    },
+    {
+        "question": "In welchem Meer liegt die Insel Hawaii?",
+        "answer_1": "Atlantischer Ozean",
+        "answer_2": "Indischer Ozean",
+        "answer_3": "Karibisches Meer",
+        "answer_4": "Pazifischer Ozean",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie heißt die Hauptstadt von Thüringen?",
+        "answer_1": "Magdeburg",
+        "answer_2": "Erfurt",
+        "answer_3": "Dresden",
+        "answer_4": "Potsdam",
+        "right_answer": 2
+    }
+];
+let questions_literature = [
+    {
+        "question": "In welcher Stadt spielt Romeo und Julia?",
+        "answer_1": "Florenz",
+        "answer_2": "Neapel",
+        "answer_3": "Rom",
+        "answer_4": "Verona",
+        "right_answer": 4
+    },
+    {
+        "question": "Was ist ein Oxymoron?",
+        "answer_1": "Ein innerer Widerspruch",
+        "answer_2": "Ein Versfuß",
+        "answer_3": "Eine Wiederholung",
+        "answer_4": "Eine Frageform",
+        "right_answer": 1
+    },
+    {
+        "question": "Welche Adresse ist mit Sherlock Holmes verbunden?",
+        "answer_1": "221b Baker Street",
+        "answer_2": "Abbey Road 42",
+        "answer_3": "Downing Street 10",
+        "answer_4": "Princess Street 7",
+        "right_answer": 3
+    }
+];
